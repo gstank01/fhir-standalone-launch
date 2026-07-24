1d0f8291b6a01dfe5fcc0f722cf19c39dca8bb49
UP2306676 <118078017+gstank01@users.noreply.github.com>
2026-07-24 15:10:26 +0100

    Fix export statement for generateRandomString function

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 // --- RANDOM STRING GENERATOR (For the randomly generated state that will be append it to each subsequent exchange in the workflow for you to validate session integrity) ---
-export function generateRandomString(length = 32) { //determine the length of the string 
+function generateRandomString(length = 32) { //determine the length of the string 
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';//allowed characters
     const values = new Uint8Array(length);//allocate byte storage
     crypto.getRandomValues(values); //uses the underlying OS entropy sources (hardware noise, system interrupts) to generate truly unpredictable values.
