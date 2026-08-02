35985fdb8cc69bb62352e1ec1ac82818580d4c73
UP2306676 <118078017+gstank01@users.noreply.github.com>
2026-08-02 20:19:23 +0100

    Fix comments in utils.js for better clarity
    Corrected comments for clarity and consistency.

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-// --- RANDOM STRING GENERATOR (For the randomly generated state that will be append it to each subsequent exchange in the workflow for you to validate session integrity) ---
+// --- Random string generator (For the randomly generated state that will be appended to each subsequent exchange in the workflow to validate session integrity) ---
 function generateRandomString(length = 32) { //determine the length of the string 
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';//allowed characters
     const values = new Uint8Array(length);//allocate byte storage
@@ -6,10 +6,27 @@ function generateRandomString(length = 32) { //determine the length of the strin
     return Array.from(values).map(x => possible[x % possible.length]).join(''); //Map random bytes to characters and return the result
 }
 
-// --- LOGGING HELPER ---
+// --- Logging helper ---
 function log(message) {
     const div = document.getElementById('logs');
     if (!div) return;
     div.innerHTML += `<div>[${new Date().toLocaleTimeString()}] ${message}</div>`;
     div.scrollTop = div.scrollHeight;
 }
+
+// --- 1. CALLBACK HANDLER (Runs inside Pop-up when redirected back from EHR) ---
+
+(function handleCallback() {
+    const urlParams = new URLSearchParams(window.location.search); //search the returned URL to extract the code paramether and match the state parametre with the one we sent out
+    const code = urlParams.get('code'); //code
+    const returnedState = urlParams.get('state'); //state
+
+    if (code && window.opener) { // check that a code was received and that the session is opened in a pop-up window
+        window.opener.postMessage({ 
+            type: 'AUTH_CODE', 
+            code: code, 
+            state: returnedState 
+        }, window.location.origin);
+        window.close();
+    }
+})();
