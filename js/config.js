704f3e38c1349cde1f4690c490cef9ee5052de19
UP2306676 <118078017+gstank01@users.noreply.github.com>
2026-08-04 09:02:38 +0100

    Update configuration placeholders in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 // config.js
 const CONFIG = {
     // Epic Sandbox Configuration
-    CLIENT_ID: 'Your non-production Client ID',
-    FHIR_BASE_URL: 'Your FHIR Base URL',
-    AUTH_URL: 'FHIR Server autorization endpoin',
-    TOKEN_URL: 'FHIR Server token endpoint',
+    CLIENT_ID: 'Replace with your non-production Client ID',
+    FHIR_BASE_URL: 'Replace with the FHIR Server Base URL',
+    AUTH_URL: 'Replace with the FHIR Server autorization endpoin',
+    TOKEN_URL: 'Replace with the FHIR Server token endpoint',
     
     // Dynamically generate Redirect URI pointing back to current page path
     REDIRECT_URI: 'https://gstank01.github.io/fhir-standalone-launch/index.html',
